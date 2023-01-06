8b2adb329e80210ee76a0ec449dfd062774f2b45
Paolo Gomez <peweegardner248@gmail.com>
2023-01-06 11:37:15 -0800

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -12,3 +12,11 @@
 
 import random
 print(" Hi there! Welcome to the one and only Number Guessing Game!")
+
+# Pick a number between 1 and 10
+
+# It is higher!
+
+# You got it! It took you 2 tries
+
+# Closing game, see you next time!
