b84d7bc20edb586bee424ece582ea8145736851a
Paolo Gomez <peweegardner248@gmail.com>
2023-01-11 09:44:33 -0800

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -9,32 +9,32 @@ def begin_playing():
     attempt_limit = 7
     
 # I should be continuously promptred for a guess until I get it right
-  while True:
-        try:
-            guess_attempt += 1
-            attempt_limit -= 1
-            guess= int(input("Think of a number falling in the range of 1 to 10: "))
-        except ValueError:
-            print("Trying guessing with a number ONLY. ")
-            continue
-        # After an incorrect guess I should be told if my answer if higher or lower than the answer
-        if guess > 10 or guess < 1:
-            print(" Range has to be from 1 to 10. Try again! ")
-            attempt_limit += 1
-            continue
-        if attempt_limit == 0 and guess != num_range:
-            print (" Sorry, you lost. Feel free to try again trmo! ")
-        if guess > num_range:
-            print(" Try lower like the sea ")
-            continue
-        if guess < num_range:
-            print(" Try higher like the sky ")
-            continue
-        else:
-            # After the game ends I should be shown my number of attempts at guessing
-            # When game ends, an ending message is shown to player
-            print("Yay, you got it! It took you {} tries.").format(guess_attempt)
-            break
+    while True:
+            try:
+                guess_attempt += 1
+                attempt_limit -= 1
+                guess= int(input("Think of a number falling in the range of 1 to 10: "))
+            except ValueError:
+                print("Trying guessing with a number ONLY. ")
+                continue
+            # After an incorrect guess I should be told if my answer if higher or lower than the answer
+            if guess > 10 or guess < 1:
+                print(" Range has to be from 1 to 10. Try again! ")
+                attempt_limit += 1
+                continue
+            if attempt_limit == 0 and guess != num_range:
+                print (" Sorry, you lost. Feel free to try again trmo! ")
+            if guess > num_range:
+                print(" Try lower like the sea ")
+                continue
+            if guess < num_range:
+                print(" Try higher like the sky ")
+                continue
+            else:
+                # After the game ends I should be shown my number of attempts at guessing
+                # When game ends, an ending message is shown to player
+                print("Yay, you got it! It took you {} tries.").format(guess_attempt)
+                break
 
 begin_playing()
 
